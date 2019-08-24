720dae0eb90c3b81b14edcf260c4227675984434
Brandon <brandon.lantz@icloud.com>
2019-08-24 12:18:03 -0600

    firebase init

--- functions/index.js
+++ functions/index.js
@@ -6,9 +6,3 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-
-exports.sendMessage = functions.https.onRequest((request, response) => {
-    var body = JSON.stringify(request.query)
-    console.log(body)
-    response.end()
-})
