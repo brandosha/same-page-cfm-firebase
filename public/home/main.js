c7485cec921d0463966233b25c693f5a3b18ff4c
Brandon <brandon.lantz@icloud.com>
2019-09-04 07:46:21 -0600

    Update home page to query groups

--- public/home/main.js
+++ public/home/main.js
@@ -1,12 +1,36 @@
+var db
+var groups = localStorage.getItem('groups')
+groups = groups ? JSON.parse(groups) : []
+
 document.addEventListener('DOMContentLoaded', function() {
     firebase.auth().onAuthStateChanged(user => {
-        if (!user) { 
+        if (!user) {
             window.location.replace('/sign-up')
-        } else {
-            loading.hide()
         }
 
-        console.log(user.uid)
+        db = firebase.firestore()
+        
+        var membersGroup = db.collectionGroup('members')
+        var memberQuery = membersGroup.where('userId', '==', user.uid)
+        .get()
+        .then(snapshot => {
+            if (snapshot.size == groups.size) {
+                loading.hide()
+                return
+            }
+
+            groups = []
+            snapshot.forEach(docSnapshot => {
+                groups.push(docSnapshot.ref.parent.parent.id)
+            })
+
+            localStorage.setItem('groups', JSON.stringify(groups))
+            loading.hide()
+        })
+        .catch(error => {
+            console.error(error)
+            loading.hide()
+        })
     })
 })
 
