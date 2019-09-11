ec5e2b82a9fa37dec0aafaef23d6d0e69e6b2601
Brandon <brandon.lantz@icloud.com>
2019-09-10 18:02:20 -0600

    Trim messages before sending

--- public/resources/FirebaseHandler.js
+++ public/resources/FirebaseHandler.js
@@ -220,7 +220,6 @@ class FirebaseHandler {
 
     async refreshMessages(groupId) {
         if (this.dataObj.offline) return
-
         if (!this.groupExists(groupId)) {
             throw new Error('No group with id ' + groupId)
         }
@@ -250,6 +249,8 @@ class FirebaseHandler {
     }
 
     async sendMessage(text, groupId) {
+        var text = text.trim()
+
         if (this.dataObj.offline) return
         if (text.length <= 0 || typeof text !== 'string') return
         if (!this.groupExists(groupId)) {
