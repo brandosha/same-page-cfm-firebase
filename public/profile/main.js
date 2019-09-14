11bb180fbff02095fd7d43b03e63b5acbb13fd4d
Brandon <brandon.lantz@icloud.com>
2019-09-14 12:51:28 -0600

    Create profile editing page

---
+++ public/profile/main.js
@@ -0,0 +1,59 @@
+window.onload = function() {
+    const auth = firebase.auth()
+    auth.onAuthStateChanged(user => {
+        if (!user) {
+            window.location.replace('/sign-in')
+            return
+        }
+
+        myUid = user.uid
+
+        handleUI()
+    })
+}
+
+var mainUI
+var myUid
+
+async function handleUI() {
+    var firestore = firebase.firestore()
+    var profileDoc = await firestore.doc('users/' + myUid).get()
+    var profileData = profileDoc.data()
+
+    mainUI = new Vue({
+        el: '#vue-main',
+        data: {
+            name: profileData.name,
+            editing: false
+        },
+        methods: {
+            saveEdits: function() {
+                loader.show()
+
+                if (this.canSave) {
+                    firestore.doc('users/' + myUid).set({
+                        name: this.name.trim()
+                    })
+                    .then(_ => {
+                        location.href = '/home'
+                    })
+                }
+            }
+        },
+        computed: {
+            canSave: function() {
+                return this.name.trim().length > 0 &&
+                    this.name !== profileData.name
+            }
+        },
+        watch: {
+            'editing': function() {
+                if (!this.editing) {
+                    this.name = profileData.name
+                }
+            }
+        }
+    })
+
+    loader.hide()
+}
