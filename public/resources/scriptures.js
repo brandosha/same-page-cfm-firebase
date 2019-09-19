733556a96e1d0c9a3ca6096a5c0a8d4679b9d94d
Brandon <brandon.lantz@icloud.com>
2019-09-19 13:59:18 -0600

    Add support for the complete Book of Mormon to parsing data

--- public/resources/scriptures.js
+++ public/resources/scriptures.js
@@ -4,5 +4,89 @@ var scriptures = [
         chapters: 22,
         verses: [20, 24, 31, 22, 6, 22, 38, 6, 22, 36, 23, 42, 30, 36, 39, 55, 25, 24, 22, 26, 31],
         path: '/bofm/1-ne/'
+    },
+    {
+        name: '2 nephi',
+        chapters: 33,
+        verses: Array(33).fill(1000),
+        path: '/bofm/2-ne/'
+    },
+    {
+        name: 'jacob',
+        chapters: 7,
+        verses: Array(7).fill(1000),
+        path: '/bofm/jacob/'
+    },
+    {
+        name: 'enos',
+        chapters: 1,
+        verses: Array(1).fill(1000),
+        path: '/bofm/enos/'
+    },
+    {
+        name: 'jarom',
+        chapters: 1,
+        verses: Array(1).fill(1000),
+        path: '/bofm/jarom/'
+    },
+    {
+        name: 'omni',
+        chapters: 1,
+        verses: Array(1).fill(1000),
+        path: '/bofm/omni/'
+    },
+    {
+        name: 'words of mormon',
+        chapters: 1,
+        verses: Array(1).fill(1000),
+        path: '/bofm/w-of-m/'
+    },
+    {
+        name: 'mosiah',
+        chapters: 29,
+        verses: Array(29).fill(1000),
+        path: '/bofm/mosiah/'
+    },
+    {
+        name: 'alma',
+        chapters: 63,
+        verses: Array(63).fill(1000),
+        path: '/bofm/alma/'
+    },
+    {
+        name: 'helaman',
+        chapters: 16,
+        verses: Array(16).fill(1000),
+        path: '/bofm/hel/'
+    },
+    {
+        name: '3 nephi',
+        chapters: 30,
+        verses: Array(30).fill(1000),
+        path: '/bofm/3-ne/'
+    },
+    {
+        name: '4 nephi',
+        chapters: 1,
+        verses: Array(1).fill(1000),
+        path: '/bofm/4-ne/'
+    },
+    {
+        name: 'mormon',
+        chapters: 9,
+        verses: Array(9).fill(1000),
+        path: '/bofm/morm/'
+    },
+    {
+        name: 'ether',
+        chapters: 15,
+        verses: Array(15).fill(1000),
+        path: '/bofm/ether/'
+    },
+    {
+        name: 'moroni',
+        chapters: 10,
+        verses: Array(10).fill(1000),
+        path: '/bofm/moro/'
     }
 ]
