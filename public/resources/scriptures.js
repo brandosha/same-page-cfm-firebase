e462472d6fad9d38e9fcc391fc572090a380964c
Brandon <brandon.lantz@icloud.com>
2019-09-19 15:56:23 -0600

    Add verse count data for first half of the Book of Mormon

--- public/resources/scriptures.js
+++ public/resources/scriptures.js
@@ -8,85 +8,85 @@ var scriptures = [
     {
         name: '2 nephi',
         chapters: 33,
-        verses: Array(33).fill(1000),
+        verses: [32, 30, 25, 35, 34, 18, 11, 25, 54, 25, 8, 22, 26, 6, 30, 13, 25, 22, 21, 34, 16, 6, 22, 32, 30, 33, 35, 32, 14, 18, 21, 9, 15],
         path: '/bofm/2-ne/'
     },
     {
         name: 'jacob',
         chapters: 7,
-        verses: Array(7).fill(1000),
+        verses: [19, 35, 14, 18, 77, 13, 27],
         path: '/bofm/jacob/'
     },
     {
         name: 'enos',
         chapters: 1,
-        verses: Array(1).fill(1000),
+        verses: [27],
         path: '/bofm/enos/'
     },
     {
         name: 'jarom',
         chapters: 1,
-        verses: Array(1).fill(1000),
+        verses: [15],
         path: '/bofm/jarom/'
     },
     {
         name: 'omni',
         chapters: 1,
-        verses: Array(1).fill(1000),
+        verses: [30],
         path: '/bofm/omni/'
     },
     {
         name: 'words of mormon',
         chapters: 1,
-        verses: Array(1).fill(1000),
+        verses: [18],
         path: '/bofm/w-of-m/'
     },
     {
         name: 'mosiah',
         chapters: 29,
-        verses: Array(29).fill(1000),
+        verses: Array(29).fill(77),
         path: '/bofm/mosiah/'
     },
     {
         name: 'alma',
         chapters: 63,
-        verses: Array(63).fill(1000),
+        verses: Array(63).fill(77),
         path: '/bofm/alma/'
     },
     {
         name: 'helaman',
         chapters: 16,
-        verses: Array(16).fill(1000),
+        verses: Array(16).fill(77),
         path: '/bofm/hel/'
     },
     {
         name: '3 nephi',
         chapters: 30,
-        verses: Array(30).fill(1000),
+        verses: Array(30).fill(77),
         path: '/bofm/3-ne/'
     },
     {
         name: '4 nephi',
         chapters: 1,
-        verses: Array(1).fill(1000),
+        verses: Array(1).fill(77),
         path: '/bofm/4-ne/'
     },
     {
         name: 'mormon',
         chapters: 9,
-        verses: Array(9).fill(1000),
+        verses: Array(9).fill(77),
         path: '/bofm/morm/'
     },
     {
         name: 'ether',
         chapters: 15,
-        verses: Array(15).fill(1000),
+        verses: Array(15).fill(77),
         path: '/bofm/ether/'
     },
     {
         name: 'moroni',
         chapters: 10,
-        verses: Array(10).fill(1000),
+        verses: Array(10).fill(77),
         path: '/bofm/moro/'
     }
 ]
